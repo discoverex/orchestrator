ea69e2b8212e5a671fcbed2955edaf31a3217e16
esillileu <esillileu@gmail.com>
2026-03-12 17:12:18 +0900

    chore: final cleanup of legacy files and driver updates

--- scripts/lib/drivers/python.sh
+++ scripts/lib/drivers/python.sh
@@ -4,5 +4,6 @@ run_python_tool() {
   local script_path="$1"
   shift
   require_executable "${script_path}"
-  exec python3 "${script_path}" "$@"
+  PYTHONPATH="${ROOT_DIR}/src:${ROOT_DIR}:${PYTHONPATH:-}" \
+    exec uv run python "${script_path}" "$@"
 }
